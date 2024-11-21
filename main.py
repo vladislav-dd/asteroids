df978780c78db090dda087796f1790acfcf77bda
vladislav-dd <vladislav.dudko0@gmail.com>
2024-11-21 22:23:58 +0000

    add player

--- main.py
+++ main.py
@@ -4,20 +4,26 @@
 import pygame
 
 from constants import *
+from player import *
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
+    dt = 0
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+        
+        dt = clock.tick(60)/1000
+
 
 if __name__ == '__main__':
     main()
---
+++ player.py
@@ -0,0 +1,23 @@
+import pygame
+from constants import *
+from circleshape import CircleShape
+
+class Player(CircleShape):
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        super.__init__(PLAYER_RADIUS)
+        rotation = 0
+    
+    def draw(self, screen):
+        pygame.draw.polygon(screen,"white", self.triangle(), width=2)
+
+        # in the player class
+    def triangle(self):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
+        a = self.position + forward * self.radius
+        b = self.position - forward * self.radius - right
+        c = self.position - forward * self.radius + right
+        return [a, b, c]
+
